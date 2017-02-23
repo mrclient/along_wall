8a3ab3d011b882bb6d417502dc5dd69016e1e48d
Evgeniy Antonov <antonovspostbox@gmail.com>
2017-02-23 16:59:58 +0300

    Add correction of need_dist into .py node

--- scripts/along_wall.py
+++ scripts/along_wall.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import rospy
+from math import cos, pi
 
 if __name__=="__main__":
 
@@ -9,3 +10,4 @@ if __name__=="__main__":
     forw_vel = rospy.get_param("~forward_velocity", 1.0)
     kp = rospy.get_param("~controller_coefficient", 1.0)
     need_dist = rospy.get_param("~needed_distance", 2.0)
+    need_dist /= cos(pi/4)
