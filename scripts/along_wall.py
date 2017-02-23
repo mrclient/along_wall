8f42b8a07cddc32d6817eb5da4de6e0078824603
Evgeniy Antonov <antonovspostbox@gmail.com>
2017-02-23 16:50:11 +0300

    Add params to .py node

--- scripts/along_wall.py
+++ scripts/along_wall.py
@@ -5,3 +5,7 @@ import rospy
 if __name__=="__main__":
 
     rospy.init_node("along_wall_node)
+
+    forw_vel = rospy.get_param("~forward_velocity", 1.0)
+    kp = rospy.get_param("~controller_coefficient", 1.0)
+    need_dist = rospy.get_param("~needed_distance", 2.0)
